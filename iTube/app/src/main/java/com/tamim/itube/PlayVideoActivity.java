177b8aa9aedba71219db25d9b791ec37789dd298
tamimNumrah <tamim@numrah.com>
2023-05-12 03:05:37 +1000

    Youtube video play implemented

--- iTube/app/src/main/java/com/tamim/itube/PlayVideoActivity.java
+++ iTube/app/src/main/java/com/tamim/itube/PlayVideoActivity.java
@@ -1,14 +1,59 @@
 package com.tamim.itube;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class PlayVideoActivity extends AppCompatActivity {
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//https://github.com/PierfrancescoSoffritti/android-youtube-player
+
+public class PlayVideoActivity extends AppCompatActivity {
+    YouTubePlayerView youtubePlayerView;
+    String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_video);
+        youtubePlayerView = findViewById(R.id.youtubePlayerView);
+        Intent intent = getIntent();
+        url = (String) getIntent().getSerializableExtra("url");
+        getLifecycle().addObserver(youtubePlayerView);
+        youtubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                YouTubeHelper helper = new YouTubeHelper();
+                String videoId = helper.extractVideoIdFromUrl(url);
+                System.out.println("videoId: "+videoId+" url: "+url);
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
+    }
+    public String extractYTId(String ytUrl) {
+        String vId = null;
+        Pattern pattern = Pattern.compile(
+                "^https?://.*(?:youtu.be/|v/|u/\\w/|embed/|watch?v=)([^#&?]*).*$",
+                Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(ytUrl);
+        if (matcher.matches()){
+            vId = matcher.group(1);
+        }
+        return vId;
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
